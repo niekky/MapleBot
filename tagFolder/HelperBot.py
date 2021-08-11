86d235602f80cddcb683c8aeb6f6eac2e67e3ed9
peterlqs <nplongquan@gmail.com>
2021-08-11 16:06:57 +0700

    done all

--- tagFolder/HelperBot.py
+++ tagFolder/HelperBot.py
@@ -1,9 +1,21 @@
+import pyperclip
+
+list1 = []
+list2 = []
 count = 0
 while True:
+    print("-----------------------")
     s = input()
+    list1.append(s)
     listz = []
     for i in s.split():
         listz.append("o")
-    print(str(listz).replace("\'","\""))
+    result = str(listz).replace("\'","\"")
+    print(result)
+    pyperclip.copy(result)
+    final = input()
+    list2.append(final)
     count+=1
     print(count)
+    print(str(list1).lstrip("[").rstrip("]").replace("\'","\""))
+    print(str(list2)[1:-1].replace("\'",""))
