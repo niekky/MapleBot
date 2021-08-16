69fb6f51e7febb8e6b880c94929c687abd8fd647
peterlqs <nplongquan@gmail.com>
2021-08-16 14:49:55 +0700

    +20

--- EntitiesTagger/tagFolder/HelperBot.py
+++ EntitiesTagger/tagFolder/HelperBot.py
@@ -1,21 +1,22 @@
 import pyperclip
 
-list1 = []
-list2 = []
 count = 0
+this_list = []
 while True:
-    print("-----------------------")
-    s = input()
-    list1.append(s)
-    listz = []
-    for i in s.split():
-        listz.append("o")
-    result = str(listz).replace("\'","\"")
-    print(result)
-    pyperclip.copy(result)
-    final = input()
-    list2.append(final)
-    count+=1
-    print(count)
-    print(str(list1).lstrip("[").rstrip("]").replace("\'","\""))
-    print(str(list2)[1:-1].replace("\'",""))
+    o_list = []
+    sentence = input("Your sentence below : \n")
+    for letter in sentence.split():
+        o_list.append("o")
+        pyperclip.copy(str(o_list))
+    print("Paste and edit below : ")
+    tag = input()
+    final = {"sentence": sentence,
+    "tag": tag}
+    this_list.append(final)
+    print("--------------------------")
+    print(str(this_list)[1:-1].replace("\"","").replace("\'","\""))
+    print("--------------------------")
+    count += 1
+    print("Total : ",count)
+
+
